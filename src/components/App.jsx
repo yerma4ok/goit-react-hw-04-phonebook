632faaaf042cabecc0d75f13a7262ccc30a227aa
yerma4ok <yerma4ok13@gmail.com>
2023-02-12 23:51:35 +0100

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import {  useState, useEffect  } from 'react';
 import { nanoid } from 'nanoid';
 import FormAddContact from './FormAddContact';
 import SectionWrap from './SectionWrap';
@@ -6,81 +6,59 @@ import ContactsList from './ContactsList';
 import FilterByName from './FilterByName';
 import { MainTitle, ContactTitle } from './App.styled';
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contactsList = localStorage.getItem('contactsList');
-    const parsedContactsList = JSON.parse(contactsList);
-
-    if (parsedContactsList) {
-      this.setState({ contacts: parsedContactsList });
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    const nextContactsList = this.state.contacts;
-    const prevContactsList = prevState.contacts;
-
-    if (nextContactsList !== prevContactsList) {
-      console.log(
-        'Обновилось поле contactsList, записываю ContactsList в хранилище'
-      );
-      localStorage.setItem('contactsList', JSON.stringify(nextContactsList));
-    }
-  }
-
-  addContact = ({ name, number }) => {
+  const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
 
-    const isExist = this.state.contacts.find(contact => contact.name === name);
+    const isExist = contacts.find(contact => contact.name === name);
 
     isExist
       ? alert(`${name} is already in the contacts`)
-      : this.setState(({ contacts }) => ({
-          contacts: [contact, ...contacts],
-        }));
+      : setContacts([contact, ...contacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
   };
 
-  getSortedContacts = contacts => {
-    return contacts.sort((x, y) => x.name.localeCompare(y.name));
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-    const filteredContacts = this.getFilteredContacts(contacts);
-    const sortedContacts = this.getSortedContacts(filteredContacts);
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
+  const sortedContacts = filteredContacts.sort((x, y) =>
+    x.name.localeCompare(y.name)
+  );
+
+  useEffect(() => {
+    const contactsList = localStorage.getItem('contactsList');
+    const parsedContactsList = JSON.parse(contactsList);
+    if (!parsedContactsList) {
+      return;
+    }
+    setContacts(parsedContactsList);
+  }, []);
+
+  useEffect(() => {
+    if (contacts.length === 0) {
+      return;
+    }
+    localStorage.setItem('contactsList', JSON.stringify(contacts));
+  }, [contacts]);
+
     return (
       <>
         <SectionWrap>
@@ -98,6 +76,3 @@ class App extends Component {
       </>
     );
   }
-}
-
-export default App;
